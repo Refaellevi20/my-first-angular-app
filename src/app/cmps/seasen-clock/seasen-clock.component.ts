9c62f6e4fc1e75639f96bd15848b22efcd852eba
Refael Levi <refael14levi@gmail.com>
2024-12-10 11:34:21 +0200

    fixed bug of img

--- src/app/cmps/seasen-clock/seasen-clock.component.ts
+++ src/app/cmps/seasen-clock/seasen-clock.component.ts
@@ -1,18 +1,18 @@
-import { Component,OnDestroy } from '@angular/core'
+import { Component, OnDestroy } from '@angular/core'
 import { TimeService } from '../../time.service'
-
 @Component({
   selector: 'app-seasen-clock',
   standalone: false,
-  
+
   templateUrl: './seasen-clock.component.html',
   styleUrl: './seasen-clock.component.scss'
 })
-export class SeasenClockComponent implements OnDestroy{
+export class SeasenClockComponent implements OnDestroy {
   realTime: Date = new Date()
   customTime: Date = new Date()
   secondsElapsed: number = 0
   isDark: boolean = false
+  imgSrc: string = ''
 
   constructor(private timeService: TimeService) {
     this.timeService.realTime$.subscribe(realTime => {
@@ -21,15 +21,28 @@ export class SeasenClockComponent implements OnDestroy{
 
     this.timeService.customTime$.subscribe(customTime => {
       this.customTime = customTime
+      this.updateImgSrc()
     })
 
     this.timeService.secondsElapsed$.subscribe(secondsElapsed => {
       this.secondsElapsed = secondsElapsed
     })
+
+
+
   }
+
+  sprint = 'https://cdn.britannica.com/05/155405-050-F8969EE6/Spring-flowers-fruit-trees-bloom.jpg'
+  winter = 'https://c.tadst.com/gfx/600x337/winter-lake.jpg?1'
+  summer = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcRtoG7gC6YolX5jiY8I4UkiD8xQ9P9kyvi6Yg&s'
+  autumn = 'https://cdn.britannica.com/88/137188-050-8C779D64/Boston-Public-Garden.jpg'
 
   ngOnDestroy(): void {
     this.timeService.stop()
+  }
+
+  ngOnInit(): void {
+    this.updateImgSrc()
   }
 
   onToggleDark() {
@@ -37,10 +50,14 @@ export class SeasenClockComponent implements OnDestroy{
   }
 
   getSeason(month: number): string {
-    if (month >= 2 && month <= 4) return 'Spring'
-    if (month >= 5 && month <= 7) return 'Summer'
-    if (month >= 8 && month <= 10) return 'Autumn'
-    return 'Winter'
+    if (month >= 2 && month <= 4) return this.sprint
+    if (month >= 5 && month <= 7) return this.summer
+    if (month >= 8 && month <= 10) return this.autumn
+    return this.winter
+  }
+
+  updateImgSrc(): void {
+    this.imgSrc = this.getSeason(this.customTime.getMonth())
   }
 
   getMonthName(monthIndex: number): string {
